c41d9ba904cd3236d70487e297aaf2cddad35664
RaphBen <raph_ben@hotmail.com>
2024-05-13 14:39:16 +0200

    [CHANGED] Set site & base

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,8 @@ import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+	site: 'https://raphben69.github.io',
+	base: 'AstroDBTest',
 	integrations: [
 		db(),
 		tailwind({
